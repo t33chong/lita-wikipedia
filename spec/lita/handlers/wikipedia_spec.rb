9ef4fc1ef217de629f94dcf3f3d6af6c7f8d5124
Tristan Chong <tristanchong@gmail.com>
2014-05-01 23:10:36 -0700

    write spec for routes and return first paragraph

--- spec/lita/handlers/wikipedia_spec.rb
+++ spec/lita/handlers/wikipedia_spec.rb
@@ -1,4 +1,13 @@
 require "spec_helper"
 
 describe Lita::Handlers::Wikipedia, lita_handler: true do
+  it { routes_command("wikipedia").to(:wikipedia) }
+  it { routes_command("wiki").to(:wikipedia) }
+
+  describe "#wikipedia" do
+    it "returns the 1st paragraph of the Wikipedia entry for a given query" do
+      send_command "wikipedia ruby language"
+      expect(replies.first).to match 'Ruby is a dynamic, reflective, object-oriented, general-purpose programming language. It was designed and developed in the mid-1990s by Yukihiro "Matz" Matsumoto in Japan.'
+    end
+  end
 end
