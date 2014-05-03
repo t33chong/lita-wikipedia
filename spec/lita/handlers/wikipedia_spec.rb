9d4a21009be735e73255b9862c53c3240eaa5f2a
Tristan Chong <tristanchong@gmail.com>
2014-05-03 16:38:29 -0700

    update test to reflect method change from wikipedia to query_wikipedia

--- spec/lita/handlers/wikipedia_spec.rb
+++ spec/lita/handlers/wikipedia_spec.rb
@@ -1,10 +1,10 @@
 require "spec_helper"
 
 describe Lita::Handlers::Wikipedia, lita_handler: true do
-  it { routes_command("wikipedia").to(:wikipedia) }
-  it { routes_command("wiki").to(:wikipedia) }
+  it { routes_command("wikipedia").to(:query_wikipedia) }
+  it { routes_command("wiki").to(:query_wikipedia) }
 
-  describe "#wikipedia" do
+  describe "#query_wikipedia" do
     it "returns the 1st paragraph & link to the Wikipedia entry for a query" do
       send_command "wikipedia ruby language"
       expect(replies[0]).to match 'Ruby is a dynamic, reflective, object-oriented, general-purpose programming language. It was designed and developed in the mid-1990s by Yukihiro "Matz" Matsumoto in Japan.'
