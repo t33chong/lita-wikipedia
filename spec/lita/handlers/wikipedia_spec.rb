b382d365b1793f64eaef6324b62a241a75c96ae8
Tristan Chong <tristanchong@gmail.com>
2014-05-02 20:57:59 -0700

    add test for link

--- spec/lita/handlers/wikipedia_spec.rb
+++ spec/lita/handlers/wikipedia_spec.rb
@@ -5,9 +5,10 @@ describe Lita::Handlers::Wikipedia, lita_handler: true do
   it { routes_command("wiki").to(:wikipedia) }
 
   describe "#wikipedia" do
-    it "returns the 1st paragraph of the Wikipedia entry for a given query" do
+    it "returns the 1st paragraph & link to the Wikipedia entry for a query" do
       send_command "wikipedia ruby language"
-      expect(replies.first).to match 'Ruby is a dynamic, reflective, object-oriented, general-purpose programming language. It was designed and developed in the mid-1990s by Yukihiro "Matz" Matsumoto in Japan.'
+      expect(replies[0]).to match 'Ruby is a dynamic, reflective, object-oriented, general-purpose programming language. It was designed and developed in the mid-1990s by Yukihiro "Matz" Matsumoto in Japan.'
+      expect(replies[1]).to match 'http://en.wikipedia.org/wiki/Ruby_(programming_language)'
     end
   end
 end
