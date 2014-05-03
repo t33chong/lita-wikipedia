207a55a1b18e55fb3c8972f7dd43b7871e162ec5
Tristan Chong <tristanchong@gmail.com>
2014-05-02 23:05:38 -0700

    write route

--- lib/lita/handlers/wikipedia.rb
+++ lib/lita/handlers/wikipedia.rb
@@ -1,6 +1,11 @@
 module Lita
   module Handlers
     class Wikipedia < Handler
+      route(/^(?:wiki|wikipedia)\s+(.*)/i, :wikipedia, command: true, help: {
+        t("help.wikipedia_key") => t("help.wikipedia_value")})
+
+      def wikipedia(response)
+      end
     end
 
     Lita.register_handler(Wikipedia)
