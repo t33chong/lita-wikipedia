a4c4fd76ddc7108af256166f597d654bf4b932a6
Tristan Chong <tristanchong@gmail.com>
2014-05-03 17:33:57 -0700

    was trying to CGI.escape array; fixed to escape string instead

--- lib/lita/handlers/wikipedia.rb
+++ lib/lita/handlers/wikipedia.rb
@@ -9,7 +9,7 @@ module Lita
             help: { t("help.wikipedia_key") => t("help.wikipedia_value")})
 
       def wikipedia(response)
-        url = "http://en.wikipedia.org/w/api.php?action=query&prop=extracts|info&format=json&exintro=&explaintext=&inprop=url&titles=#{CGI.escape(response.matches.first)}&redirects="
+        url = "http://en.wikipedia.org/w/api.php?action=query&prop=extracts|info&format=json&exintro=&explaintext=&inprop=url&titles=#{CGI.escape(response.matches.first.first)}&redirects="
         result = JSON.parse(open(URI.parse(URI.encode(url.strip))).read)
         page = result['query']['pages'].first[1]
         response.reply(page['extract'].split("\n").first)
