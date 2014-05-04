635a69d61c949693adf1c18076cbc509b714a389
Tristan Chong <tristanchong@gmail.com>
2014-05-03 19:20:15 -0700

    add disambiguate function

--- lib/lita/handlers/wikipedia.rb
+++ lib/lita/handlers/wikipedia.rb
@@ -2,6 +2,21 @@ require "cgi"
 require "json"
 require "open-uri"
 
+def disambiguate(term)
+  url = "http://en.wikipedia.org/w/api.php?action=query&prop=extracts|info|links&format=json&exintro=&explaintext=&inprop=url&titles=#{CGI.escape(term)}&redirects="
+  result = JSON.parse(open(URI.parse(URI.encode(url.strip))).read)
+  page = result['query']['pages'].first[1]
+  extract = page['extract'].split("\n").first
+  if extract.include? 'may refer to'
+    links = page['links'].map {
+      |x| x['title'] if not x['title'].downcase.include? 'disambiguation'}
+    links = links.select {|x| x != nil}
+    return disambiguate(links.sample)
+  end
+  url = page['fullurl']
+  [extract, url]
+end
+
 module Lita
   module Handlers
     class Wikipedia < Handler
@@ -9,11 +24,9 @@ module Lita
             help: { t("help.wikipedia_key") => t("help.wikipedia_value")})
 
       def wikipedia(response)
-        url = "http://en.wikipedia.org/w/api.php?action=query&prop=extracts|info&format=json&exintro=&explaintext=&inprop=url&titles=#{CGI.escape(response.matches.first.first)}&redirects="
-        result = JSON.parse(open(URI.parse(URI.encode(url.strip))).read)
-        page = result['query']['pages'].first[1]
-        response.reply(page['extract'].split("\n").first)
-        response.reply("Source: #{page['fullurl']}")
+        extract, url = disambiguate(response.matches.first.first)
+        response.reply(extract)
+        response.reply("Source: #{url}")
       end
     end
 
