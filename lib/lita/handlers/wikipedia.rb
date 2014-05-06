3a8739f882f29e82194e0a32d269be461f68917f
Tristan Chong <tristanchong@gmail.com>
2014-05-05 20:52:42 -0700

    make disambiguate a private method of Wikipedia

--- lib/lita/handlers/wikipedia.rb
+++ lib/lita/handlers/wikipedia.rb
@@ -1,24 +1,6 @@
 require "json"
 require "open-uri"
 
-def disambiguate(term)
-  url = "http://en.wikipedia.org/w/api.php?action=query&prop=extracts|info|links|pageprops&format=json&exintro=&explaintext=&inprop=url&ppprop=disambiguation&titles=#{term}&redirects="
-  result = JSON.parse(open(URI.parse(URI.encode(url.strip))).read)
-  page = result['query']['pages'].first[1]
-  if not page.has_key? 'extract'
-    return ["No Wikipedia entry found for '#{term}'.", nil]
-  end
-  extract = page['extract'].split("\n").first
-  if page.fetch('pageprops', {}).has_key? 'disambiguation'
-    links = page['links'].map {
-      |x| x['title'] if not x['title'].downcase.include? 'disambiguation'}
-    links = links.select {|x| x != nil}
-    return disambiguate(links.sample)
-  end
-  url = page['fullurl']
-  [extract, url]
-end
-
 module Lita
   module Handlers
     class Wikipedia < Handler
@@ -31,6 +13,25 @@ module Lita
         if url != nil
           response.reply("Source: #{url}")
         end
+      end
+
+      private
+      def disambiguate(term)
+        url = "http://en.wikipedia.org/w/api.php?action=query&prop=extracts|info|links|pageprops&format=json&exintro=&explaintext=&inprop=url&ppprop=disambiguation&titles=#{term}&redirects="
+        result = JSON.parse(open(URI.parse(URI.encode(url.strip))).read)
+        page = result['query']['pages'].first[1]
+        if not page.has_key? 'extract'
+          return ["No Wikipedia entry found for '#{term}'.", nil]
+        end
+        extract = page['extract'].split("\n").first
+        if page.fetch('pageprops', {}).has_key? 'disambiguation'
+          links = page['links'].map {
+            |x| x['title'] if not x['title'].downcase.include? 'disambiguation'}
+          links = links.select {|x| x != nil}
+          return disambiguate(links.sample)
+        end
+        url = page['fullurl']
+        [extract, url]
       end
     end
 
