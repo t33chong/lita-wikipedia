8c30e66464cb2a34357dd9870e1907beb2d0148f
Tristan Chong <tristanchong@gmail.com>
2015-06-12 23:27:20 -0700

    lib: https, clean up code

--- lib/lita/handlers/wikipedia.rb
+++ lib/lita/handlers/wikipedia.rb
@@ -1,31 +1,41 @@
-require "json"
 require "nokogiri"
-require "open-uri"
 require "sanitize"
 
 module Lita
   module Handlers
     class Wikipedia < Handler
+
+      API_URL = "https://en.wikipedia.org/w/api.php"
+
       route(/^(?:wiki|wikipedia)\s+(.*)/i, :wikipedia, command: true,
             help: { t("help.wikipedia_key") => t("help.wikipedia_value")})
 
       def wikipedia(response)
-        term = response.matches.first.first
-        search_url = "http://en.wikipedia.org/w/api.php?action=opensearch&search=#{term}&format=json"
-        search_result = JSON.parse(open(URI.parse(URI.encode(search_url.strip))).read)
-        titles = search_result[1]
-        if titles.empty?
-          response.reply("No Wikipedia entry found for '#{term}'")
-        else
-          query_url = "http://en.wikipedia.org/w/api.php?action=query&prop=extracts|info&format=json&exintro=&inprop=url&titles=#{titles.first}&redirects="
-          query_result = JSON.parse(open(URI.parse(URI.encode(query_url.strip))).read)
-          page = query_result['query']['pages'].first[1]
-          html = Nokogiri::HTML(page['extract'])
-          extract = Sanitize.fragment(html.xpath('//p[1]').first).strip
-          url = page['fullurl']
-          response.reply(extract)
-          response.reply("Source: #{url}")
-        end
+        term = response.matches[0][0]
+        search = http.get(
+          API_URL,
+          search: term,
+          action: "opensearch",
+          format: "json"
+        )
+        titles = MultiJson.load(search.body)[1]
+        return response.reply("No Wikipedia entry found for '#{term}'") if titles.empty?
+        query = http.get(
+          API_URL,
+          titles: titles[0],
+          action: "query",
+          prop: "extracts|info",
+          exintro: nil,
+          inprop: "url",
+          redirects: nil,
+          format: "json"
+        )
+        page = MultiJson.load(query.body)["query"]["pages"].values[0]
+        html = Nokogiri::HTML(page["extract"])
+        extract = Sanitize.fragment(html.xpath("//p[1]")[0]).strip
+        url = page["fullurl"]
+        response.reply(extract)
+        response.reply("Source: #{url}")
       end
 
     end
